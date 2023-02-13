3ea9b62ab8fdf8414a16a9f7e3069d3da6893e24
ivan-pl <pleskaiv@gmail.com>
2023-02-13 11:15:32 +0300

    feat: adds nth-child

--- src/getPath.test.ts
+++ src/getPath.test.ts
@@ -1,6 +1,6 @@
 import getPath from "./getPath";
 
-const appendElements = (elements: HTMLElement[]): void =>
+const appendElements = (...elements: HTMLElement[]): void =>
   elements.forEach((el) => document.body.append(el));
 
 const checkExpectations = (el: HTMLElement, cssSelector: string): void => {
@@ -14,7 +14,17 @@ describe("getPath()", () => {
   it("returns div", () => {
     const div = document.createElement("div");
     const p = document.createElement("p");
-    appendElements([div, p]);
+    appendElements(div, p);
     checkExpectations(div, "div");
+  });
+
+  it("returns li:nth-child(n)", () => {
+    const ul = document.createElement("ul");
+    const li1 = document.createElement("li");
+    const li2 = document.createElement("li");
+    ul.append(li1, li2);
+    appendElements(ul);
+    checkExpectations(li1, "li:nth-child(1)");
+    checkExpectations(li2, "li:nth-child(2)");
   });
 });
--- src/getPath.ts
+++ src/getPath.ts
@@ -1,3 +1,35 @@
 export default function getPath(el: HTMLElement): string {
-  return "div";
+  const curEl = el;
+  const uniqueSelector = "";
+  do {
+    const curSelector = curEl.nodeName.toLowerCase();
+    let newSelector = curSelector + uniqueSelector;
+
+    if (isUniqueSelector(newSelector)) return newSelector;
+
+    const nthChildClass = getNthChildSelector(curEl, newSelector);
+    if (nthChildClass) {
+      newSelector = curSelector + nthChildClass;
+      if (isUniqueSelector(newSelector)) return newSelector;
+    }
+  } while (!isUniqueSelector(uniqueSelector));
+
+  return uniqueSelector;
+}
+
+function isUniqueSelector(cssSelector: string): boolean {
+  return document.querySelectorAll(cssSelector).length === 1;
+}
+
+function getNthChildSelector(
+  childEl: HTMLElement,
+  cssSelector: string
+): string {
+  const parent = childEl.parentElement;
+  if (!parent || parent.querySelectorAll(cssSelector).length === 1) {
+    return "";
+  }
+
+  const ind = [...parent.childNodes].indexOf(childEl);
+  return `:nth-child(${ind + 1})`;
 }
